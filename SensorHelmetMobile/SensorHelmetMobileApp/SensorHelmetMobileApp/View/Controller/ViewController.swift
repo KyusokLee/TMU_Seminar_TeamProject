32ec08213e3be3d00ee654fae09f99e80321ee96
KyusokLee <kyusok1@naver.com>
2022-12-18 23:29:18 +0900

    update to implement main VC

--- SensorHelmetMobile/SensorHelmetMobileApp/SensorHelmetMobileApp/View/Controller/ViewController.swift
+++ SensorHelmetMobile/SensorHelmetMobileApp/SensorHelmetMobileApp/View/Controller/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var serialMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // serialの初期化
+        serial = BluetoothSerial.init()
     }
+    
+    @IBAction func scanButtonAction(_ sender: Any) {
+        let scanVC = UIStoryboard.init(name: "ScanView", bundle: nil).instantiateViewController(withIdentifier: "ScanVC")
+        self.present(scanVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func sendMessageButtonAction(_ sender: Any) {
+        if !serial.isReadyToUseBluetooth {
+            print("Serial is not ready!")
+            return
+        }
+        serial.delegate = self
+        // messageを設定し、これに繋がったperipheralに伝送するメソッドを呼び出す
+        let msg = "okokok"
+        serial.sendMessageToDevice(msg)
+        // ラベルのtextを変更し、データを待ち中であることを表現する
+        serialMessageLabel.text = "waiting for Peripheral's messege"
+    }
+}
 
-
+extension ViewController: BluetoothSerialDelegate {
+    func serialDidReceiveMessage(message: String) {
+        // 返信で返ってきたメッセージをラベルに表示
+        serialMessageLabel.text = message
+    }
+    
 }
 
