4a80c12b94634173242cf7064a337ca5403cc9bc
TakumiSenaha <117294735+TakumiSenaha@users.noreply.github.com>
2022-12-29 19:56:00 +0900

    Create constans.js

---
+++ path/to/map/constans.js
@@ -0,0 +1,4 @@
+//for create sheet2gmap at https://github.com/champierre/sheet2gmap/blob/master/constants.js
+
+const API_KEY = 'API キーを入力してください';
+const SHEET_ID = 'Google スプレッドシートの Sheet ID を入力してください';
