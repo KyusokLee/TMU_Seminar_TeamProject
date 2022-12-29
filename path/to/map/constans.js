33ca5939b922d68eccc68d496fd3c41908766990
TakumiSenaha <117294735+TakumiSenaha@users.noreply.github.com>
2022-12-29 19:57:50 +0900

    add gss key

--- path/to/map/constans.js
+++ path/to/map/constans.js
@@ -1,4 +1,4 @@
 //for create sheet2gmap at https://github.com/champierre/sheet2gmap/blob/master/constants.js
 
 const API_KEY = 'API キーを入力してください';
-const SHEET_ID = 'Google スプレッドシートの Sheet ID を入力してください';
+const SHEET_ID = '1fDsMCqDCY61gp_hn_uEdMqr-ho85GtrsPeqxvmcgymI';
