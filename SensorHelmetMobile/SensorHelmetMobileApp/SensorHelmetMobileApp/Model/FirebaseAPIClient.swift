9ee0382dc540966e2ca6fc28ad1b8bb3e0de90e5
KyusokLee <kyusok1@naver.com>
2023-01-02 23:24:55 +0900

    firebase api 更新

---
+++ SensorHelmetMobile/SensorHelmetMobileApp/SensorHelmetMobileApp/Model/FirebaseAPIClient.swift
@@ -0,0 +1,75 @@
+//
+//  FirebaseAPIClient.swift
+//  SensorHelmetMobileApp
+//
+//  Created by Kyus'lee on 2023/01/02.
+//
+
+import Foundation
+
+struct GoogleVisonAPIClient: GoogleVisonAPIClientProtocol {
+    // typealiasの指定
+//    typealias NetworkCompletion = (_ data: Data?, _ error: Error?) -> Void
+    
+    // API仕様はこちらを参照
+    // https://cloud.google.com/vision/docs/ocr?hl=ja
+    // https://cloud.google.com/vision/docs/reference/rest/v1/images/annotate?hl=ja#AnnotateImageRequest
+    func send(base64String: String, completion: @escaping ((Data?, Error?) -> Void)) {
+        // TODO: 課題2
+        // ここでCloud Vision APIのリクエストを組み立て
+        // URLSessionを使って通信をする
+        // 通信が終わったらcompletionを呼ぶこと
+        // API Key: AIzaSyBGokVmQrGhd9PdYWUDRCKPSM6y-smmdcM
+        // 現在有効ではない
+        let request = buildRequest(with: base64String)
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }.resume()
+    }
+}
+
+// requestをbuildするところは、extensionで作成した方がいいかも
+
+private extension GoogleVisonAPIClient {
+    func buildRequest(with base64String: String) -> URLRequest {
+        // TODO: 課題2
+        // ここでCloud Vision APIのリクエストを組み立て
+        // URLSessionを使って通信をする
+        // 通信が終わったらcompletionを呼ぶこと
+        let googleApiKey = ""
+        let url = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(googleApiKey)")!
+        // 上記のsendのメソッドで DataTaskの completionを行うので、ここで再びif let bindingをする必要はない
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let params: [String: AnyObject] = [
+            "requests": [
+                "image": [
+                    "content":base64String
+                ],
+                "features": [[
+                    "type": "DOCUMENT_TEXT_DETECTION"
+                ]]
+            ] as AnyObject
+        ]
+        
+        request.httpBody = try! JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+        return request
+        
+        // do catch 文を使うためには、throwsを使わないといけない
+//        do {
+//            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+//            return request
+//        } catch let e as NSError {
+//            print("Error: \(e.localizedDescription)")
+//        }
+    }
+}
+
+protocol GoogleVisonAPIClientProtocol {
+    func send(base64String: String, completion: @escaping ((Data?, Error?) -> Void))
+}
