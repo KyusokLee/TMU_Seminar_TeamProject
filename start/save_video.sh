2967ff800b146b5845742b249d7688b868cc3e43
TakumiSenaha <117294735+TakumiSenaha@users.noreply.github.com>
2023-01-10 15:21:45 +0900

    Update save_video.sh

--- start/save_video.sh
+++ start/save_video.sh
@@ -1,5 +1,14 @@
-##capture camera footage and move avi files
-python3 /home/zemi/start/python_code/ip_ad.py
+##capture camera footage and move mp4 files
+##1~5分おきに実行
+
+##動画を保存
 python3 /home/zemi/start/python_code/webcam_to_php.py
-sudo mv /home/zemi/start/python_code/video/*avi /var/www/html/camera/video
+
+##動画をアップロード
+python3 /home/zemi/start/python_code/uploadToFirebase.py
+##アップロードに成功したら実行するコマンド
+##sudo mv /home/zemi/start/python_code/video/* /var/www/html/camera/video
+
+##常にPHPでwebページに出力するファイルは30個に設定しておく
+sudo find /var/www/html/camera/video/*  -maxdepth 0 | sort | head -n -30 | xargs rm -rf
 
