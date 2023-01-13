d5977ea46155ad372313ae081388105a2baf1d8d
TakumiSenaha <117294735+TakumiSenaha@users.noreply.github.com>
2023-01-13 16:41:42 +0900

    Update savePicture.py
    hidden path

--- path/savePicture.py
+++ path/savePicture.py
@@ -12,19 +12,18 @@ import subprocess
 def get_jpg(capture):
     now = datetime.datetime.now()
     # ファイル保存用の設定
-    #save_dir = "/var/www/html/camera/pictures"
-    save_dir = "/home/zemi/start/python_code/picture/"
+    #save_dir = ""
+    save_dir = "folder for storing Image"
     f_name = now.strftime("%Y年%m月%d日%H時%M分") + ".jpg"
-    f_name = os.path.join("/home/zemi/start/python_code/picture/",f_name)
+    f_name = os.path.join("[folder for storing Image]",f_name)
     rr, img = capture.read()
     cv2.imwrite(f_name, img)
     capture.release()
-    # 撮影＝ループ中にフレームを1枚ずつ取得（qキーで撮影終了）
+    # 撮影＝ループ中にフレームを1枚ずつ取得
    
 
 def main():
-    #url = "http://192.168.151.126:8080/?action=stream"
-    #url = "http://192.168.0.37:8080/?action=stream"
+    #url = "http://[IP ad of RasPi]/?action=stream"
     flg = False
     try:
         connect_interface = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -41,25 +40,25 @@ def main():
         print(t)
         #mjpg-streamerを停止
         print("stop_mjpg-streamer")
-        subprocess.run(["/home/zemi/start/stop_mjpg-streamer.sh", "arguments"], shell=True)
+        subprocess.run(["Shell Script path to stop mjpg-streamer", "arguments"], shell=True)
         video_ = cv2.VideoCapture(0)
         flg = True
-    #mjpg-streamerが起動していない場合
+    #mjpg-streamerが起動していない場合か or なんらかのエラー
     except (URLError, urlib.error.URLError, ConnectionError, ConnectionRefusedError) as e:
         t = e.__class__.__name__
         print(t)
         print("no url")
         #get_video(video_2)
-        #subprocess.run(["/home/zemi/start/stop_mjpg-streamer.sh", "arguments"], shell=True)
+        subprocess.run(["Shell Script path to stop mjpg-streamer", "arguments"], shell=True)
         time.sleep(1)
         video_ = cv2.VideoCapture(0)
-        #subprocess.run(["/home/zemi/start/autostart.sh", "arguments"], shell=True)
+        subprocess.run(["Shell Script path to start mjpg-streamer", "arguments"], shell=True)
         flg = True
     finally:
         get_jpg(video_)
         time.sleep(3)
-        #if (flg == True):
-            #subprocess.run(["/home/zemi/start/autostart.sh", "arguments"], shell=True)
+        if (flg == True):
+            subprocess.run(["Shell Script path to start mjpg-streamer", "arguments"], shell=True)
             
             
 if __name__ == '__main__':
